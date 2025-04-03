c0dbd6adfa96582a837ad1f0cbf750c277053e21
BIIIGBULL <101936014+BIIIGBULL@users.noreply.github.com>
2025-04-03 22:23:46 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    images:{
+        domains:[
+            "kpepiahuhutfgpruvsve.supabase.co"
+        ]
+    }
+
+};
+
+export default nextConfig;
